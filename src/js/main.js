57ec3d2263076843ff82ad93327c5abadb984349
Sascha <79339263+donbronson98@users.noreply.github.com>
2022-12-23 19:56:00 +0100

    added current tech stack

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+//Make 'Scroll to top' visible
+scrollFunction = function() {
+    let second_section = document.getElementById('second-section').getBoundingClientRect();
+    let y = window.scrollY;
+    console.log(y);
+    if(y > second_section.top) {
+        document.getElementById('arrow-up-container').style.width = '32px';
+        document.getElementById('arrow-up-container').style.height = '32px';
+        document.getElementById('arrow-up-container').style.opacity = '1';
+    } else {
+        document.getElementById('arrow-up-container').style.width = '0';
+        document.getElementById('arrow-up-container').style.height = '0';
+        document.getElementById('arrow-up-container').style.opacity = '0';
+    }
+}
+window.addEventListener("scroll", scrollFunction);
